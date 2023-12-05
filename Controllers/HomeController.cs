e71ebec4159518169c32e98edb2e81aef6c96383
Brian Scherner <brianscherner@gmail.com>
2023-12-05 15:12:34 -0800

    Add functionality to display an API call.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,20 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MvcApiCall.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace MvcApiCall.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+  private readonly string _apikey;
+  public HomeController(IConfiguration configuration)
+  {
+    _apikey = configuration["NYT"];
+  }
+  public IActionResult Index()
+  {
+    List<Article> allArticles = Article.GetArticles(_apikey);
+    return View(allArticles);
+  }
 }
